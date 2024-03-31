eaa731b2bb22076c3270f61b1948854aed3e1a02
adityacyan <cntsgg@gmail.com>
2024-04-01 01:46:52 +0530

    added Container3.jsx updated main.jsx

---
+++ src/Container3.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import poiner from './assets/pointer.svg'
+
+
+function Container3() {
+    return (
+        <div>
+            <h1>
+                Innovate With Speed            </h1>
+            <h3>
+                Create higher quality software, deliver on customer expectations and business goals            </h3>
+            <p>
+                DevOps
+            </p>
+            <img src={poiner} alt="pointer image"/>
+
+        </div>
+    )
+
+}
+
+export default Container3
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import Header from './Header.jsx'
 import './index.css'
 import Container1 from "./Container1.jsx";
 import Container2 from "./Container2.jsx";
+import Container3 from "./Container3.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Container1 />
       <Container2 />
+      <Container3/>
   </React.StrictMode>,
 )
