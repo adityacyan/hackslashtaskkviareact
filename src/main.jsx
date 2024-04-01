3fbd89bcfaa31f519bfae0e1839c5e1344cd6c78
adityacyan <cntsgg@gmail.com>
2024-04-01 10:01:24 +0530

    modified:   src/Body.jsx 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header.jsx'
 import './index.css'
-import Container1 from "./Container1.jsx";
-import Container2 from "./Container2.jsx";
-import Container3 from "./Container3.jsx";
+
+import Body from "./Body.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <Container1 />
-      <Container2 />
-      <Container3/>
+    <Body />
   </React.StrictMode>,
 )
