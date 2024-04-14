0951a90bcf9757c5b9abd026926bca39bd67a884
adityacyan <cntsgg@gmail.com>
2024-04-14 14:39:19 +0530

    created container 4

---
+++ src/Container4.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import poiner from './assets/pointer.svg'
+
+
+function Container4() {
+    return (
+        <div>
+            <h1>
+                Embrace Cloud
+            </h1>
+            <p>
+                With Cloud-First accelerate innovation and optimize performance            </p>
+            <h3>
+                Cloud Services
+
+            </h3>
+            <img src={poiner} alt="pointer image" />
+
+        </div>
+    )
+
+}
+
+export default Container4
